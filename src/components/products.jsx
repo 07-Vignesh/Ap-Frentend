23ce2548a7aaeb8b41f9ab5fc59dc573b770d865
Viknesh <2006vigneshvicky@gmail.com>
2025-06-14 12:39:45 +0530

    Update products.jsx

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -187,7 +187,7 @@ const { isSignedIn } =  useAuth();
             {( isSignedIn ==true) &&(
               <div className="card-actions justify-end ">
                 {!isAdmin &&(
-                <button className="btn btn-accent my-2 mr-26  " onClick={() => handleAddToCart(item)}>
+                <button className="btn btn-accent my-2 mr-39 " onClick={() => handleAddToCart(item)}>
     Add to Cart
   </button>)}
 
@@ -202,7 +202,7 @@ const { isSignedIn } =  useAuth();
               {( isSignedIn==false) &&(
               <div className="card-actions justify-end">
                 <Link to="/login">
-                 <button className="btn btn-accent my-2 mr-28  " onClick={() => handleAddToCart(item)}>
+                 <button className="btn btn-accent my-2 mr-39  " onClick={() => handleAddToCart(item)}>
       Add to Cart
     </button>
        
