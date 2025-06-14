3e9ff95e020fc23ae26328e198db800673acc5a5
Viknesh <2006vigneshvicky@gmail.com>
2025-06-14 12:37:01 +0530

    Update birds.products.jsx

--- src/components/birds.products.jsx
+++ src/components/birds.products.jsx
@@ -191,7 +191,7 @@ const { isSignedIn } =  useAuth();
           {( isSignedIn ==true) &&(
             <div className="card-actions justify-end ">
               {!isAdmin &&(
-              <button className="btn btn-accent my-2 mr-26  " onClick={() => handleAddToCart(item)}>
+              <button className="btn btn-accent my-2 mr-39  " onClick={() => handleAddToCart(item)}>
   Add to Cart
 
 </button>)}
@@ -208,7 +208,7 @@ const { isSignedIn } =  useAuth();
             {( isSignedIn==false) &&(
             <div className="card-actions justify-end">
               <Link to="/login">
-      <button className="btn btn-accent my-2 mr-28  " onClick={() => handleAddToCart(item)}>
+      <button className="btn btn-accent my-2 mr-39  " onClick={() => handleAddToCart(item)}>
       Add to Cart
     </button>
      {(!isAdmin&&
